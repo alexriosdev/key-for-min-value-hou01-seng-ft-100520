c2afeaf3f9c382066df04be7bc83ccdfd5b486cc
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-17 06:01:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 
   min_val = 10000
-  min_key = ""
+  min_key = nil
   name_hash.each do |key, value|
     if name_hash[key][value] <= min_val
       min_val = name_hash[key]
